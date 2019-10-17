7c2ef3903925681be497c267027b30ecf2a18e28
lutzid <lut.zidan@gmail.com>
2019-10-17 11:50:11 +0700

    create new internal match migration

---
+++ database/migrations/2019_10_17_044723_create_pertandingan_internal.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePertandinganInternal extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pertandingan_internal', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('id_name');
+            $table->string('name');
+            $table->integer('seed');
+            $table->integer('round');
+            $table->integer('score');
+            $table->string('foto1')->nullable();
+            $table->string('foto2')->nullable();
+            $table->string('foto3')->nullable();
+            $table->string('foto4')->nullable();
+            $table->string('foto5')->nullable();
+            $table->string('foto6')->nullable();
+            $table->integer('fk_operator_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pertandingan_internal');
+    }
+}
